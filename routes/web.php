713a08a263091cf2af3f4f7925ce696f4f1d219d
jaqsyit <romzikarduino@gmail.com>
2026-03-18 14:53:08 +0500

    second change

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,21 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Models\User;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -17,30 +23,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::post('/call/offer', function(Illuminate\Http\Request $r){
-    broadcast(new \App\Events\CallOfferSent($r->to, auth()->id(), $r->sdp));
-    return response()->noContent();
-})->middleware('auth');
-
-Route::post('/call/answer', function(Illuminate\Http\Request $r){
-    broadcast(new \App\Events\CallAnswerSent($r->to, auth()->id(), $r->sdp));
-    return response()->noContent();
-})->middleware('auth');
-
-Route::post('/call/candidate', function(Illuminate\Http\Request $r){
-    broadcast(new \App\Events\IceCandidateSent($r->to, auth()->id(), $r->candidate));
-    return response()->noContent();
-})->middleware('auth');
-
-Route::middleware('auth')->get('/video/{peer}', function (User $peer) {
-    return view('call', ['peerId' => $peer->id, 'peer' => $peer]);
-});
-
-Route::middleware('auth')->get('/contacts', function () {
-    $users = \App\Models\User::where('id', '!=', auth()->id())->get();
-    return view('contacts', compact('users'));
-});
-
 
 require __DIR__.'/auth.php';
